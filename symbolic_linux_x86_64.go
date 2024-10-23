acc49e29ae0f94498b361b4688d9c8dad39657f9
Martin Holman <me@martinholman.co.nz>
2024-10-23 10:53:29 -0700

    Match output

--- symbolic_linux_x86_64.go
+++ symbolic_linux_x86_64.go
@@ -3,8 +3,8 @@
 package symbolic
 
 // #cgo LDFLAGS: -lsymbolic_cabi
-// #cgo LDFLAGS: -L${SRCDIR}/lib/linux_amd64
+// #cgo LDFLAGS: -L${SRCDIR}/lib/linux_x86_64
 // #cgo LDFLAGS: -Wl,-rpath,$ORIGIN
 // #cgo LDFLAGS: -Wl,-rpath,$ORIGIN/../lib
-// #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/linux_amd64
+// #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/linux_x86_64
 import "C"
