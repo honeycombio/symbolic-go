dd647e8fbd5a9c81b75f3aa7a9895cea5352a12f
Martin Holman <me@martinholman.co.nz>
2024-10-31 16:00:19 -0700

    Fix paths

--- symbolic_linux_arm64.go
+++ symbolic_linux_arm64.go
@@ -3,8 +3,8 @@
 package symbolic
 
 // #cgo LDFLAGS: -lsymbolic_cabi
-// #cgo LDFLAGS: -L${SRCDIR}/lib/linux_arm64
+// #cgo LDFLAGS: -L${SRCDIR}/lib/linux_aarch64
 // #cgo LDFLAGS: -Wl,-rpath,$ORIGIN
 // #cgo LDFLAGS: -Wl,-rpath,$ORIGIN/../lib
-// #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/linux_arm64
+// #cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/lib/linux_aarch64
 import "C"
